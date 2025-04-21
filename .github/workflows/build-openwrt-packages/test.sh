42152ff5e23d8dc30afd4eb2c133f8ad8886c933
Christopher Söllinger <christopher.soellinger@dig.com>
2025-04-21 09:38:28 +0200

    another test

--- .github/workflows/build-openwrt-packages/test.sh
+++ .github/workflows/build-openwrt-packages/test.sh
@@ -6,5 +6,8 @@ mkdir -p /openwrt-bin/"$SHORT_VER"
 touch /openwrt-bin/1.txt
 touch /openwrt-bin/2.txt
 
+echo "Mein kleiner Text 1" > /openwrt-bin/1.txt
+echo "Mein kleiner Text 2" > /openwrt-bin/2.txt
+
 # cp -v "$KMOD_FILE" /openwrt-bin/"$SHORT_VER"/
 # cp -v "$DAEMON_FILE" /openwrt-bin/"$SHORT_VER"/
